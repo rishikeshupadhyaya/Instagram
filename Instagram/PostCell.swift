758221abf45ed99ec155b40da8800ed584310ca6
Rishikesh Upadhyaya <90358040+rishikeshupadhyaya@users.noreply.github.com>
2021-10-27 23:33:33 -0400

    Add files via upload

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Rishikesh Upadhyaya on 10/25/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
